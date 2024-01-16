bd1ff9139a12784958e48cb575bea8064ebf305d
armandorodrigues2023 <armando.rodrigues@vecttor.es>
2024-01-16 11:40:49 +0100

    add VivecApiClient

---
+++ Vrt.Vivec.Svc/Clients/Vivec/VivecApiClient.cs
@@ -0,0 +1,32 @@
+﻿namespace Vrt.Vivec.Svc.Clients.Vivec
+{
+    public class VivecApiClient : HttpClient
+    {
+        private readonly IConfiguration _configuration;
+
+        public VivecApiClient(IConfiguration configuration)
+        {
+            _configuration = configuration;
+
+            BaseAddress = new Uri(_configuration.GetValue<string>("Vivec:BaseUrl"));
+
+            Timeout = new TimeSpan(0, 10, 0);
+
+            DefaultRequestHeaders.TryAddWithoutValidation("Authorization", _configuration.GetValue<string>("Payflow:Key"));
+        }
+
+        public async Task<string> SendRequest(HttpRequestMessage request)
+        {
+            var response = await SendAsync(request);
+            int tries = 0;
+
+            while (!response.EnsureSuccessStatusCode().IsSuccessStatusCode && tries < 3)
+            {
+                tries++;
+                response = await SendAsync(request);
+            }
+
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
